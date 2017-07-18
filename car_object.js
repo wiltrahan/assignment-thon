24b44cd9e42aca564f2b0bdb7d3b546ec0ef836f
wiltrahan <twilorip@gmail.com>
2017-07-18 19:16:09 -0400

    log message incl model type

--- car_object.js
+++ car_object.js
@@ -7,28 +7,35 @@
 
 rentals = {
   sedan: {
+    model: 'sedan',
     cost: 59,
     numAvail: 5,
-    available: true
+    available: true,
   },
   suv: {
+    model: 'suv',
     cost: 79,
     numAvail: 4,
     available: true
   },
   sports: {
+    model: 'sports',
     cost: 99,
     numAvail: 0,
     available: false
   },
   request: function(type) {
     if (type.available === true) {
-      console.log("This vehicle is available to rent! The cost is " + type.cost + " dollars and there are " + type.numAvail + " available.");
+      console.log("The " + type.model + " is available to rent! The cost is " + type.cost + " dollars, and there are " + type.numAvail + " available.");
+
     } else {
       console.log("Sorry, this model is not available.");
     }
   }
 };
 
+rentals.request(rentals.sedan);
 rentals.request(rentals.suv);
+rentals.request(rentals.sports);
+
 
